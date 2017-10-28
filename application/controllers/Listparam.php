bc36ec57766e192bf0c97499fc51be72ebd81e3c
tommi asmara <tommi.asmara3@gmail.com>
2017-10-28 16:28:02 +0700

    penambahan service parameter-parameter

--- application/controllers/Listparam.php
+++ application/controllers/Listparam.php
@@ -14,4 +14,54 @@ class ListParam extends Controlapi{
     $data = $this->mp->mst_relation();
     $this->ok($data);
   }
+
+    public function list_mst_cancel_reason_get(){
+    $data = $this->mp->mst_cancel_reason();
+    $this->ok($data);
+  }
+
+  public function list_mst_insr_provider_get(){
+    $data = $this->mp->mst_insr_provider();
+    $this->ok($data);
+  }
+
+  public function list_mst_payment_methode_get(){
+    $data = $this->mp->mst_payment_methode();
+    $this->ok($data);
+  }
+
+  public function list_mst_service_type_get(){
+    $data = $this->mp->mst_service_type();
+    $this->ok($data);
+  }
+
+  public function list_mst_partner_type_get(){
+    $data = $this->mp->mst_partner_type();
+    $this->ok($data);
+  }
+
+  public function list_mst_spesialisasi_get(){
+    $data = $this->mp->mst_spesialisasi();
+    $this->ok($data);
+  }
+
+  public function list_mst_dosis_obat_get(){
+    $data = $this->mp->mst_dosis_obat();
+    $this->ok($data);
+  }
+
+  public function list_mst_prescription_type_get(){
+    $data = $this->mp->mst_prescription_type();
+    $this->ok($data);
+  }
+
+  public function list_mst_use_instruction_get(){
+    $data = $this->mp->mst_use_instruction();
+    $this->ok($data);
+  }
+
+  public function list_mst_satuan_obat_get(){
+    $data = $this->mp->mst_satuan_obat();
+    $this->ok($data);
+  }
 }
--- application/models/Modelparam.php
+++ application/models/Modelparam.php
@@ -16,4 +16,84 @@ class ModelParam extends CI_Model{
     return $query->result();
   }
 
+    public function mst_cancel_reason() {
+    $this->db->select('cancel_reason_id, cancel_reason_desc');
+    $this->db->from('mst_cancel_reason');
+    $this->db->where('is_active','Y');
+    $query = $this->db->get();
+    return $query->result();
+  }
+
+public function mst_insr_provider() {
+    $this->db->select('insr_provider_id, insr_provider_desc');
+    $this->db->from('mst_insr_provider');
+    $this->db->where('is_active','Y');
+    $query = $this->db->get();
+    return $query->result();
+  }
+
+  public function mst_payment_methode() {
+    $this->db->select('pymt_methode_id, pymt_methode_desc');
+    $this->db->from('mst_payment_methode');
+    $this->db->where('is_active','Y');
+    $query = $this->db->get();
+    return $query->result();
+  }
+
+  public function mst_service_type() {
+    $this->db->select('service_type_id, service_type_desc');
+    $this->db->from('mst_service_type');
+    $this->db->where('is_active','Y');
+    $query = $this->db->get();
+    return $query->result();
+  }
+
+  public function mst_partner_type() {
+    $this->db->select('partner_type_id, partner_type_desc');
+    $this->db->from('mst_partner_type');
+    $this->db->where('is_active','Y');
+    $query = $this->db->get();
+    return $query->result();
+  }
+
+  public function mst_spesialisasi() {
+    $this->db->select('spesialisasi_id, spesialisasi_desc');
+    $this->db->from('mst_spesialisasi');
+    $this->db->where('is_active','Y');
+    $query = $this->db->get();
+    return $query->result();
+  }
+
+  public function mst_dosis_obat() {
+    $this->db->select('dosis_obat_id, dosis_obat_desc');
+    $this->db->from('mst_dosis_obat');
+    $this->db->where('is_active','Y');
+    $query = $this->db->get();
+    return $query->result();
+  }
+
+  public function mst_prescription_type() {
+    $this->db->select('prescription_type_id, prescription_type_desc');
+    $this->db->from('mst_prescription_type');
+    $this->db->where('is_active','Y');
+    $query = $this->db->get();
+    return $query->result();
+  }
+
+  public function mst_use_instruction() {
+    $this->db->select('use_instruction_id, use_instruction_desc');
+    $this->db->from('mst_use_instruction');
+    $this->db->where('is_active','Y');
+    $query = $this->db->get();
+    return $query->result();
+  }
+
+  public function mst_satuan_obat() {
+    $this->db->select('satuan_obat_id, satuan_obat_desc');
+    $this->db->from('mst_satuan_obat');
+    $this->db->where('is_active','Y');
+    $query = $this->db->get();
+    return $query->result();
+  }
+
 }
