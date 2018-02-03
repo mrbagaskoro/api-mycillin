a1943f996d4485d500797a7a4d800e5d8f286fba
Aziz <native148@gmail.com>
2018-02-03 19:16:08 +0700

    Penambahan script redirect pada controllers/Welcome.php

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,7 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		header("Location: https://www.mycillin.co.id/");
+		die();
 	}
 }
